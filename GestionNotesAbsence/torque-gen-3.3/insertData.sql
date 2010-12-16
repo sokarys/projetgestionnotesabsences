3383d44d578d8d1f6353e2b8e81e2fe0b0ca1ffe
michael.lefevre38 <michael.lefevre38@5d895b15-b7a9-f1db-1730-95a7ac035f01>
2010-12-16 14:26:16 +0000

    commit de la mort qui tue

--- GestionNotesAbsence/torque-gen-3.3/insertData.sql
+++ GestionNotesAbsence/torque-gen-3.3/insertData.sql
@@ -2,23 +2,29 @@ INSERT INTO matiere (nom, description, prof)
        VALUES  ('combat','combat contre les force du mal','blanchon'),
                 ('repos','cours pour apprendre a se reposer','brunet-manquat'),
                 ('SI','apprentissage de l utilite des SI','coat');
-INSERT INTO classe (nom, etudiant_id, matiere_id)
-       VALUES  ('Hero',1,1),
-               ('Semi-Hero',3,3),
-               ('Boulet',4,2);
-INSERT INTO Etudiant (nom, prenom)
-       VALUES  ('Super', 'Man'),
-                ('Bat' , 'Man'),
-                ('Spider', 'Man'),
-                ('Super', 'Touriste');
+INSERT INTO classe (nom)
+       VALUES  ('Hero'),
+               ('Semi-Hero'),
+               ('Boulet');
+INSERT INTO etudiant (nom, prenom,classe_id)
+       VALUES  ('Super', 'Man',1),
+                ('Bat' , 'Man',1),
+                ('Spider', 'Man',2),
+                ('Super', 'Touriste',3);
 INSERT INTO absence (dateDebut, dateFin, motif, etudiant_id)
        VALUES  (CURRENT_DATE,CURRENT_DATE, 'Sauver le monde',1),
                (CURRENT_DATE,CURRENT_DATE, 'Sauver un chat',2),
                (CURRENT_DATE,CURRENT_DATE, 'Morsure',3);
-
-
-
-INSERT INTO controle (coef,date,note,matiere_id,etudiant_id)
-       VALUES  (5,CURRENT_DATE,15,3,1),
-                (5,CURRENT_DATE,17,3,2),
-                (5,CURRENT_DATE,5,3,3);
+INSERT INTO cours (matiere_id,classe_id)
+       VALUES  (1,1),
+                (3,1),
+                (1,2),
+                (2,2),
+                (2,3);
+INSERT INTO controle (coef,date,cours_id)
+       VALUES  (5,CURRENT_DATE,1),
+                (10,CURRENT_DATE,4);
+INSERT INTO note (note,id_controle,etudiant_id)
+       VALUES  (20,1,1),
+                (15,1,2),
+                (17,2,4);
