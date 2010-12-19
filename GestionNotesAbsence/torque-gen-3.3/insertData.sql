8d605809f834f020801fe6be768d98f81016fb75
michael.lefevre38 <michael.lefevre38@5d895b15-b7a9-f1db-1730-95a7ac035f01>
2010-12-19 11:21:06 +0000

    insertData

--- GestionNotesAbsence/torque-gen-3.3/insertData.sql
+++ GestionNotesAbsence/torque-gen-3.3/insertData.sql
@@ -12,9 +12,9 @@ INSERT INTO etudiant (nom, prenom,classe_id)
                 ('Spider', 'Man',2),
                 ('Super', 'Touriste',3);
 INSERT INTO absence (dateDebut, dateFin, motif, etudiant_id)
-       VALUES  (CURRENT_DATE,CURRENT_DATE, 'Sauver le monde',1),
-               (CURRENT_DATE,CURRENT_DATE, 'Sauver un chat',2),
-               (CURRENT_DATE,CURRENT_DATE, 'Morsure',3);
+       VALUES  ('01/01/2000','01/01/2000', 'Sauver le monde',1),
+               ('01/01/2000','01/01/2000', 'Sauver un chat',2),
+               ('01/01/2000','01/01/2000', 'Morsure',3);
 INSERT INTO cours (matiere_id,classe_id)
        VALUES  (1,1),
                 (3,1),
@@ -22,8 +22,8 @@ INSERT INTO cours (matiere_id,classe_id)
                 (2,2),
                 (2,3);
 INSERT INTO controle (coef,date,cours_id)
-       VALUES  (5,CURRENT_DATE,1),
-               (10,CURRENT_DATE,4);
+       VALUES  (5,'01/02/2000',1),
+               (10,'01/02/2000',4);
 INSERT INTO note (note,controle_id,etudiant_id)
        VALUES  (20,1,1),
                 (15,1,2),
